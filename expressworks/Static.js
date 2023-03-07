02e9de3fe863bbe5926091afcfca6cdebf01914d
DaviHenrique30 <126525207+DaviHenrique30@users.noreply.github.com>
2023-03-06 22:52:39 -0300

    Delete expressworks directory

--- expressworks/Static.js
+++
@@ -1,7 +0,0 @@
-var express = require('express')
-var path = require('path')
-var app = express()
-
-app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
-
-app.listen(process.argv[2])
